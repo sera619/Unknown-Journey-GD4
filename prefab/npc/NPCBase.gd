401de56b2b476234438090668bab8c06fda3488e
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-03-11 01:21:50 +0100

    create NPCBase Scene and Class

--- prefab/npc/NPCBase.gd
+++ prefab/npc/NPCBase.gd
@@ -10,8 +10,15 @@ class_name NPCBase
 @export var acceleration: int
 @export var friction: int
 
-@onready var animplayer = $AnimationPlayer
-@onready var animtree = $AnimationTree 
+@export_category('NPC Quest Settings')
+@export var quest: Quest
+@export_multiline var default_text: String
+@export_multiline var quest_start_text: String
+@export_multiline var quest_ready_text: String
+@export_multiline var quest_complete_text: String
+
+@onready var animplayer: AnimationPlayer = $AnimationPlayer
+@onready var animtree: AnimationTree = $AnimationTree 
 @onready var animstate = animtree.get("parameters/playback")
 @onready var wander_controller: WanderController = $WanderController
 @onready var player_detector: PlayerDetector = $PlayerDetector
@@ -23,6 +30,7 @@ enum {
 	TALK
 }
 var state = IDLE
+var first_seen:bool = true
 
 func _setup_movement_blends():
 	pass
@@ -31,10 +39,40 @@ func update_wander():
 	state = pick_random_state([IDLE, WANDER])
 	wander_controller.start_wander_timer(randi_range(1, 3))
 
-func pick_random_state(state_list):
+func pick_random_state(state_list: Array):
 	state_list.shuffle()
 	return state_list.pop_front()
+
+func give_quest():
+	if quest and npc_type == "Quest-NPC":
+		if quest.quest_state == Quest.QS.NOT_GIVEN:
+			QuestManager.add_quest_to_log(quest)
+			QuestManager.set_current_quest(quest)
+			GameManager.ui_questlog.update_questlist()
+			print("[!] Quest-NPC \"%s\": Add Quest \"%s\" to Questlog!" % [self.npc_name, quest.quest_name])
 
 func accelerate_towards_point(point, delta):
 	var direction = global_position.direction_to(point)
 	velocity = velocity.move_toward(direction * move_speed, acceleration * delta)
+
+func wander_state(delta):
+	if wander_controller.get_time_left() == 0:
+		update_wander()
+	accelerate_towards_point(wander_controller.target_position, delta)
+	if global_position.distance_to(wander_controller.target_position) <= 4:
+		update_wander()
+
+func idle_state(delta):
+	velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	if wander_controller.get_time_left() == 0:
+		update_wander()
+
+func chase_state(delta):
+	var player = player_detector.player
+	if player != null:
+		if global_position.distance_to(player.global_position) <= 16 or global_position.distance_to(player.global_position) >= 64:
+			accelerate_towards_point(player.global_position, delta)
+		else:
+			state = IDLE
+	else:
+		state = IDLE
