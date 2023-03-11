e023587759f1f5fb55aa8d583f8e860f575803f5
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-03-11 12:22:41 +0100

    add delta to animationblends

--- prefab/npc/Chicken.gd
+++ prefab/npc/Chicken.gd
@@ -4,13 +4,13 @@ func _ready():
 	pick_random_state([IDLE, WANDER])
 	animtree.active = true
 	
-func _setup_movement_blends():
+func _setup_movement_blends(delta):
 	animtree.set("parameters/Idle/blend_position", velocity)
 	animtree.set("parameters/Move/blend_position", velocity)
 
 func _physics_process(delta):
 	if velocity != Vector2.ZERO:
-		_setup_movement_blends()
+		_setup_movement_blends(delta)
 		animstate.travel("Move")
 	else:
 		animstate.travel("Idle")
