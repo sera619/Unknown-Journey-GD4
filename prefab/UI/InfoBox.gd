2a1d4d0823a5c3ec81ded8fb4c0038fb73bd90c3
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-03-11 15:05:38 +0100

    add infobox caching

--- prefab/UI/InfoBox.gd
+++ prefab/UI/InfoBox.gd
@@ -5,12 +5,27 @@ class_name InfoBox
 @onready var text_label: Label = $BG/M/BGText/M/Label
 @onready var show_timer: Timer = $Timer
 
+var text_cache = []
+var current_text: String = ""
+
 func _ready():
 	self.visible = false
 	GameManager.register_node(self)
 
 func set_info_text(text: String):
-	text_label.text = text
+	if text_label.text != "":
+		text_cache.append(text)
+	else:
+		text_label.text = text
+		show_info_text()
+	
+func check_cache():
+	if !text_cache.is_empty():
+		text_label.text = text_cache.pop_front()
+		show_info_text()
+	else:
+		return
+
 
 func reset_info_text():
 	text_label.text = ""
@@ -25,3 +40,4 @@ func hide_info_text():
 
 func _on_timer_timeout():
 	hide_info_text()
+	check_cache()
