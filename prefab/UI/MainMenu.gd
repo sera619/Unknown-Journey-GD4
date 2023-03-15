404b138322abd406aa40ec1bdc529aaa31278dc4
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-03-15 14:15:41 +0100

    Update MainMenu.gd

--- prefab/UI/MainMenu.gd
+++ prefab/UI/MainMenu.gd
@@ -8,15 +8,10 @@ func _on_mm_start_btn_button_down():
 func _on_mm_load_btn_button_down():
 	print("[!] MainMenu: Load Btn clicked")
 	GameManager.game.load_game()
-	
-	
+
 func _on_mm_option_btn_button_down():
 	print("[!] MainMenu: Option Btn clicked")
 
-	pass # Replace with function body.
-
-
 func _on_mm_exit_btn_button_down():
 	print("[!] MainMenu: Exit Btn clicked")
-
 	get_tree().quit()
