b5cb120d637183e1640f462e46189eea42de1a20
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-03-15 18:13:15 +0100

    add random drops @ destroyableobjects

--- prefab/destroyable_objects/Gras.gd
+++ prefab/destroyable_objects/Gras.gd
@@ -1,9 +1,14 @@
 extends Node
 
+@export_category("Basic Settings")
 @export var effect_scene: PackedScene
 @export var max_health: int = 1
 @export var respawn: bool
 @export var respawn_time: int
+@export var reward_player: bool
+
+@export_category("Reward Settings")
+@export var reward_scene: PackedScene
 
 @onready var hitbox: Area2D = $Hitbox
 @onready var sprite: Sprite2D = $Sprite2D
@@ -40,3 +45,9 @@ func on_area_entered(area):
 				respawn_timer.wait_time = respawn_time
 				respawn_timer.start()
 				print("[!] GrasObject: Respawntime %s started!" % respawn_time)
+			if reward_player:
+				var random = randf_range(1, 3)
+				if random > 1.5:
+					var reward = reward_scene.instantiate()
+					reward.global_position = self.global_position
+					get_tree().current_scene.call_deferred("add_child", reward)
