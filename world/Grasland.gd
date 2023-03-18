53e7e349ffaab4bc6b074201ce9c9bcf43bf3f45
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-03-18 15:30:53 +0100

    add no weapon infotrigger to Grasland

--- world/Grasland.gd
+++ world/Grasland.gd
@@ -1,2 +1,9 @@
 extends WorldBase
 
+@onready var info_trigger = $Map/GameObjects/InformationTrigger
+
+func _ready():
+	_on_ready()
+	if GameManager.player != null:
+		if GameManager.player.stats.has_sword:
+			info_trigger.queue_free()
