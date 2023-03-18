b47753127321bc616691d088139d8a9e06c841ab
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-03-18 15:33:23 +0100

    add transistions to level switches

--- prefab/UI/MainMenu.gd
+++ prefab/UI/MainMenu.gd
@@ -3,11 +3,13 @@ class_name MainMenu
 
 func _on_mm_start_btn_button_down():
 	print("[!] MainMenu: Start Btn clicked")
-	GameManager.game.start_new_game()
-
+	EventHandler.connect("transition_black", start_new)
+	EventHandler.emit_signal("start_transition")
+	
 func _on_mm_load_btn_button_down():
 	print("[!] MainMenu: Load Btn clicked")
-	GameManager.game.load_game()
+	EventHandler.connect("transition_black", load_new)
+	EventHandler.emit_signal("start_transition")
 
 func _on_mm_option_btn_button_down():
 	print("[!] MainMenu: Option Btn clicked")
@@ -15,3 +17,11 @@ func _on_mm_option_btn_button_down():
 func _on_mm_exit_btn_button_down():
 	print("[!] MainMenu: Exit Btn clicked")
 	get_tree().quit()
+
+func start_new():
+	EventHandler.disconnect("transition_black", start_new)
+	GameManager.game.start_new_game()
+
+func load_new():
+	EventHandler.disconnect("transition_black", load_new)
+	GameManager.game.load_game()
