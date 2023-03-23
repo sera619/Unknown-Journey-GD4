70d549b4a2b3a832aacbf38f10e36375b2e38dbe
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-03-23 20:47:02 +0100

    add soundscenes to SoundController, skeleton, flap

--- prefab/utils/SoundController.gd
+++ prefab/utils/SoundController.gd
@@ -2,18 +2,49 @@ extends Node
 class_name SoundController
 
 
-@onready var move_player: AudioStreamPlayer = $MoveSound
-@onready var run_player: AudioStreamPlayer = $RunSound 
+@onready var move_player:  = $MoveSound
+@onready var run_player: = $RunSound
+@onready var impact_player:  = $ImpactPlayer 
+@onready var cast_player: = $CastPlayer
 
-const player_move_sound = "res://assets/Music and Sounds/foodsteps/HeavyDirt1.wav"
-const player_run_sound = "res://assets/Music and Sounds/foodsteps/HeavyDirtRun1.wav"
+
+const MOVEMENT: Dictionary = {
+	"Player": {
+		"Move": preload("res://assets/Music and Sounds/foodsteps/HeavyDirt1.wav"),
+		"Run": preload("res://assets/Music and Sounds/foodsteps/HeavyDirtRun1.wav")
+	},
+	"Enemys": {
+		"WingFlap": preload("res://assets/Music and Sounds/wing_fap.wav"),
+		"Mushroom": preload("res://assets/Music and Sounds/foodsteps/mushroom_step.wav"),
+		"Skeleton": preload("res://assets/Music and Sounds/foodsteps/skeleton_step.wav")
+	}
+} 
+
+
+const SPELL_CAST: Dictionary = {
+	"Ice": preload("res://assets/Music and Sounds/spells/ice_cast.wav")
+}
 
 func _setup_sounds(character: String):
 	match character:
 		"Player":
-			move_player.stream = preload(player_move_sound)
-			run_player.stream = preload(player_run_sound)
+			move_player.stream = MOVEMENT.Player.Move
+			run_player.stream =	MOVEMENT.Player.Run
+		"FlyingEnemy":
+			move_player.stream = MOVEMENT.Enemys.WingFlap
+		"Skeleton":
+			move_player.stream = MOVEMENT.Enemys.Skeleton
+			
 	print("[!] SoundController: Sound @ %s successfully loaded!" % character) 
+
+
+
+
+func _play_spell_cast_sound(element):
+	match element:
+		SkillManager.ELEMENT.ICE:
+			cast_player.stream = SPELL_CAST.Ice
+	cast_player.play()
 
 func _play_food_sound():
 	if run_player.playing:
