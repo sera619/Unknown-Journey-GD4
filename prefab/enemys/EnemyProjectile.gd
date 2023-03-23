856d1c667564beba792bbb91440071c4b249352e
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-03-23 21:33:01 +0100

    fix homeing missle behavior

--- prefab/enemys/EnemyProjectile.gd
+++ prefab/enemys/EnemyProjectile.gd
@@ -10,6 +10,8 @@ extends EnemyWeapon
 @export_enum("Direct", "Homeing") var projectile_type: String = "Direct"
 @export_enum( "None", "Fire", "Ice", "Poison", "Lightning") var spell_element: String = "None"
 @export var spell_sound_scene: PackedScene
+@export var impact_effect_scene: PackedScene
+@export var impact_sound_scene: PackedScene
 @onready var timer = $Timer
 @onready var targetDetector = $TargetDetector
 @onready var animPlayer = $AnimationPlayer
@@ -18,17 +20,18 @@ var drag_factor := 0.15 : set = set_drag_factor
 var current_velocity: Vector2 = Vector2.ZERO
 var direction: Vector2 = Vector2.ZERO
 var target = null
-
+var is_active = false
 
 	
 func _ready():
+	is_active = true
 	damage = spell_damage
 	self.set_element_type(spell_element)
 	look_at(global_position + direction)
 	knockback_vector = global_position + direction
 	self.connect("area_entered",Callable(self,"_destroy_projectile"))
 	timer.connect("timeout",Callable(self,"_on_timer_timeout"))
-	if projectile_type == "Homeing Projectile":
+	if projectile_type == "Homeing":
 		targetDetector.connect("body_entered",Callable(self,"_on_target_detector_body_entered"))
 	if is_anitamed:
 		animPlayer.play("loop")
@@ -42,26 +45,29 @@ func _play_spell_sound():
 		self.add_child(sound) 
 
 func _physics_process(delta):
-	if direction.x >= 0:
-		$Sprite2D.flip_v = false
-		$CollisionShape2D.position.x = 8
+	if is_active:
+		if direction.x >= 0:
+			$Sprite2D.flip_v = false
+			$CollisionShape2D.position.x = 8
+		else:
+			$CollisionShape2D.position.x =  -8
+			$Sprite2D.flip_v = true
+		match projectile_type: 
+			"Homeing":
+				if target != null:
+					direction = global_position.direction_to(target.global_position)
+				if direction != Vector2.ZERO:
+					var desired_velocity = direction * speed
+					var previous_velocity = current_velocity
+					var change = (desired_velocity - current_velocity) * drag_factor
+					current_velocity += change
+					position += current_velocity * delta
+					look_at(global_position + current_velocity)
+			"Direct":
+				position += direction * speed * delta
+				look_at(global_position + direction)
 	else:
-		$CollisionShape2D.position.x =  -8
-		$Sprite2D.flip_v = true
-	match projectile_type: 
-		"Homeing":
-			if target != null:
-				direction = global_position.direction_to(target.global_position)
-			if direction != Vector2.ZERO:
-				var desired_velocity = direction * speed
-				var previous_velocity = current_velocity
-				var change = (desired_velocity - current_velocity) * drag_factor
-				current_velocity += change
-				position += current_velocity * delta
-				look_at(global_position + current_velocity)
-		"Direct":
-			position += direction * speed * delta
-			look_at(global_position + direction)
+		return
 
 
 func set_drag_factor(new_value: float) -> void:
@@ -74,10 +80,25 @@ func _on_target_detector_body_entered(body):
 	if body.name != "Player":
 		return
 	else:
-		target = body.hitbox
+		target = body.hitbx
+
+func _create_impact_effect():
+	var effect = impact_effect_scene.instantiate()
+	effect.connect("effect_finished", self._kill)
+	effect.global_position = self.global_position
+	var sound = impact_sound_scene.instantiate()
+	sound.global_position = self.global_position
+	effect.add_child(sound)
+	get_tree().current_scene.add_child(effect)
+	call_deferred("_kill")
+
+
+func _kill():
+	self.call_deferred("queue_free")
+
 
 func _destroy_projectile(area):
 	if !area.is_in_group("playerHitbox"):
 		return
 	else:
-		self.call_deferred("queue_free")
+		_create_impact_effect()
