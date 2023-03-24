0567612414b82f061ffcf4bebd20786bab07765e
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-03-24 09:07:32 +0100

    enable shadow and lights @ SwordCave

--- world/SwordCave.gd
+++ world/SwordCave.gd
@@ -1,10 +1,8 @@
 extends WorldBase
 
-
-
 func _ready():
 	_on_ready()
 	if GameManager.player.stats.has_sword:
 		$Map/GameObjects/SwordDrop.queue_free()
-
-	#get_tree().call_group("lights", "activate_light")
+	EventHandler.emit_signal("show_world_shadow")
+	get_tree().call_group("lights", "activate_light")
