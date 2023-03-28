a1afe3d1e3a9e71033528da98d7eb2aad3b00abe
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-03-28 17:24:38 +0200

    setup base loadmenu

---
+++ prefab/UI/LoadMenu.gd
@@ -0,0 +1,23 @@
+extends Control
+class_name LoadMenu
+
+
+@onready var name_slot_1 =$BG/M/V/Profil1/M/H/NameLabel
+@onready var level_slot_1 = $BG/M/V/Profil1/M/H/LevelLabel
+@onready var name_slot_2 = $BG/M/V/Profil2/M/H/NameLabel
+@onready var level_slot_2 = $BG/M/V/Profil2/M/H/LevelLabel
+@onready var name_slot_3 = $BG/M/V/Profil3/M/H/NameLabel
+@onready var level_slot_3 = $BG/M/V/Profil3/M/H/LevelLabel
+
+
+func _initialize_loadmenu():
+	pass
+
+
+
+
+
+
+
+
+
