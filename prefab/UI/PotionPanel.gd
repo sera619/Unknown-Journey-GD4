ae998795f01c1c9bf7fd30454f8d167a1f6dd3b6
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-03-30 21:32:16 +0200

    Update PotionPanel.gd

--- prefab/UI/PotionPanel.gd
+++ prefab/UI/PotionPanel.gd
@@ -8,6 +8,7 @@ func _ready():
 	self.visible = false
 	EventHandler.connect("player_get_energiepot", update_energiepot)
 	EventHandler.connect("player_get_healthpot", update_healthpot)
+	EventHandler.connect("player_inventory_item_changed", update_potions)
 
 func update_healthpot(new_value):
 	healthpot_amount.text = "%s" % new_value
@@ -15,3 +16,9 @@ func update_healthpot(new_value):
 func update_energiepot(new_value):
 	energiepot_amount.text = "%s" % new_value
 
+func update_potions(item: Item):
+	match item.item_name:
+		"Heiltrank":
+			healthpot_amount.text = "%s" % item.item_amount
+		"Energietrank":
+			energiepot_amount.text = "%s" % item.item_amount
