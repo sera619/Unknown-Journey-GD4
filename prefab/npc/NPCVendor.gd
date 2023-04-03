67bd88cf2c80fe9403f839d33f7853c461f973db
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-04-03 10:55:18 +0200

    Create NPCVendor.gd

---
+++ prefab/npc/NPCVendor.gd
@@ -0,0 +1,44 @@
+extends NPCBase
+class_name NPCVendor
+
+@export_category("Items")
+@export var item_list: Array[PackedScene]
+@onready var icon: Sprite2D = $SpeakIcon
+var is_talking: bool = false
+
+
+func _ready():
+	_on_ready()
+
+func _physics_process(delta):
+	if velocity != Vector2.ZERO: 
+		animtree.set("parameters/Move/blend_position", velocity)
+		animtree.set("parameters/Cast/blend_position", velocity)
+		animtree.set("parameters/Dead/blend_position", velocity)
+		animtree.set("parameters/Hurt/blend_position", velocity)
+		animtree.set("parameters/Heal/blend_position", velocity)
+		animtree.set("parameters/Idle/blend_position", velocity)
+		animstate.travel("Move")
+	else:
+		animstate.travel("Idle")
+	
+	if player_detector.can_see_player():
+		icon.visible = true
+	else:
+		icon.visible = false
+	if Input.is_action_just_pressed("interact") and player_detector.can_see_player() and not is_talking:
+		is_talking = true
+		dialog_handler()
+
+
+func dialog_handler():
+	var dialog: DialogBox = GameManager.dialog_box
+	dialog.set_speaker(self)
+	dialog.option_a_btn.connect("button_up", open_shop)
+	dialog.set_dialog_text("Hallo mein Freund!\nMöchtest du etwas kaufen?")
+	dialog.show_dialog()
+
+func open_shop():
+	var dialog: DialogBox = GameManager.dialog_box
+	dialog.option_a_btn.disconnect("button_up", open_shop)
+	GameManager.interface.shop_panel.setup_shop(item_list)
