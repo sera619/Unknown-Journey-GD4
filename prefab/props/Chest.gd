073cd411adcccd879992508b4312d3d0d35d1297
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-04-04 16:43:39 +0200

    make chest unique

--- prefab/props/Chest.gd
+++ prefab/props/Chest.gd
@@ -7,12 +7,28 @@ class_name Chest
 @export var open_sound_scene: PackedScene
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
 @onready var player_detector: PlayerDetector = $PlayerDetector
+@onready var body_sprite: Sprite2D = $Body
 
 var is_open: bool = false
 var reward_given: bool = false
 
 func _ready():
 	anim_player.connect("animation_finished", _on_animation_finished)
+	call_deferred("_check_unique")
+
+func _check_unique():
+	await get_tree().create_timer(0.5).timeout
+	var unique_data: Dictionary = D.unique_open_data
+	print(GameManager.current_world.world_name)
+	print(unique_data)
+	for i in unique_data[str(GameManager.current_world.world_name)]:
+		if i == chest_id:
+			is_open = true
+			reward_given = true
+			body_sprite.frame = 6
+			break
+		else:
+			continue
 
 func _open_chest():
 	if is_open:
@@ -21,6 +37,7 @@ func _open_chest():
 		var sound = open_sound_scene.instantiate()
 		self.add_child(sound)
 		#await get_tree().create_timer(0.4).timeout
+		D.unique_open_data[str(GameManager.current_world.world_name)].append(chest_id)
 		is_open = true
 		anim_player.play("open")
 
