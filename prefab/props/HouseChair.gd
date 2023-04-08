e761094690126cc737b5e53f67cbed034ada26f6
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-04-08 20:07:03 +0200

    fix look direction

--- prefab/props/HouseChair.gd
+++ prefab/props/HouseChair.gd
@@ -6,9 +6,9 @@ extends StaticBody2D
 func _ready():
 	match direction:
 		0:
-			sprite.region_rect = Rect2(128, 480, 32, 32) 
-		1:
 			sprite.region_rect = Rect2(96, 480, 32, 32) 
+		1:
+			sprite.region_rect = Rect2(128, 480, 32, 32) 
 		2:
 			sprite.region_rect = Rect2(224, 400, 32, 32) 
 		3:
