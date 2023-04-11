a57940bb90018f084501f9cc25186f101d776aa4
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-04-11 15:45:25 +0200

    add questobjects @ map

--- world/CityCellar.gd
+++ world/CityCellar.gd
@@ -1,6 +1,10 @@
 extends WorldBase
 
+@onready var lava_damage = $Map/Bricks/LavaDamage
 
 func _ready():
 	_on_ready()
 	get_tree().call_group("world_light", "_light_on")
+	lava_damage.damage = 10
+
+
