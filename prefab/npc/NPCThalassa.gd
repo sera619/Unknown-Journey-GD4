5d1abc22bb64d1ddeb8f5df08c748b8509eb9598
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-04-11 18:57:28 +0200

    Create NPCThalassa.gd

---
+++ prefab/npc/NPCThalassa.gd
@@ -0,0 +1,65 @@
+extends NPCBase
+
+@onready var speak_icon: Sprite2D = $SpeakIcon
+var is_talking: bool = false
+
+func _ready():
+	_on_ready()
+
+
+func _physics_process(delta):
+	if velocity != Vector2.ZERO: 
+		animtree.set("parameters/Move/blend_position", velocity)
+		animtree.set("parameters/Cast/blend_position", velocity)
+		animtree.set("parameters/Dead/blend_position", velocity)
+		animtree.set("parameters/Hurt/blend_position", velocity)
+		animtree.set("parameters/Heal/blend_position", velocity)
+		animtree.set("parameters/Idle/blend_position", velocity)
+		animstate.travel("Move")
+	else:
+		animstate.travel("Idle")
+	
+	if player_detector.player != null:
+		speak_icon.visible = true
+	else:
+		speak_icon.visible = false
+	
+	match state:
+		IDLE:
+			idle_state(delta)
+		WANDER:
+			wander_state(delta)
+		CHASE:
+			chase_state(delta)
+	
+	if Input.is_action_just_pressed("interact") and player_detector.can_see_player() and not is_talking:
+		is_talking = true
+		dialog_handler()
+	move_and_slide()
+
+func chase_state(_delta):
+	state = IDLE
+	pass
+
+func dialog_handler():
+	var dialog: DialogBox = GameManager.dialog_box
+	dialog.set_speaker(self)
+	if not QuestManager.current_quest and not QuestManager.is_quest_availble("Ernte"):
+		if QuestManager.is_quest_availble("Danika"):
+			QuestManager.activate_quest("Danika")
+			var quest: Quest = QuestManager.current_quest
+			dialog.set_dialog_text(quest.start_text)
+			dialog.show_dialog()
+		else:
+			return
+	elif QuestManager.current_quest:
+		var quest: Quest = QuestManager.current_quest
+		if quest.title == "Danika":
+			match quest.state:
+				Quest.QS.ACTIVE:
+					dialog.set_dialog_text(quest.progress_text)
+					dialog.show_dialog()
+					
+	else:
+		dialog.set_dialog_text(default_text)
+		dialog.show_dialog()
