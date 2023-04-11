e66c998b48d6fa4e2a03b499150adb36c953ad72
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-04-11 15:42:24 +0200

    adjust chestype & transforms

--- prefab/props/Chest.gd
+++ prefab/props/Chest.gd
@@ -2,20 +2,39 @@ extends Node2D
 class_name Chest
 
 @export var chest_id: int = 1
-@export_enum("Normal", "Special") var chest_type: int
+@export_enum("Wood", "Red") var chest_type: int
 @export var reward_list: Array[PackedScene]
 @export var open_sound_scene: PackedScene
-@onready var anim_player: AnimationPlayer = $AnimationPlayer
 @onready var player_detector: PlayerDetector = $PlayerDetector
-@onready var body_sprite: Sprite2D = $Body
+@onready var body_sprite: AnimatedSprite2D = $Body
 @onready var icon: Sprite2D = $Icon
+@onready var collision: CollisionShape2D = $StaticBody2D/CollisionShape2D
+@onready var detector_collision: CollisionShape2D = $PlayerDetector/CollisionShape2D
+@onready var shadow: Sprite2D = $Shadow
 
 var is_open: bool = false
 var reward_given: bool = false
 
 
 func _ready():
-	anim_player.connect("animation_finished", _on_animation_finished)
+	match chest_type:
+		0:
+			body_sprite.animation = "wood"
+			body_sprite.offset.y = -4
+			body_sprite.frame = 0
+			collision.shape.size = Vector2(14.443, 7.159)
+			detector_collision.shape.radius = 13.5
+			shadow.scale = Vector2(0.67, 0.281)
+			shadow.position = Vector2(0, 0)
+		1:
+			body_sprite.animation = "red"
+			body_sprite.offset.y = -8
+			body_sprite.frame = 0
+			collision.shape.size = Vector2(31.865, 13.453)
+			detector_collision.shape.radius = 23.1 
+			shadow.scale = Vector2(1.287, 0.603)
+			shadow.position = Vector2(0, 1.197)
+	body_sprite.connect("animation_finished", _on_animation_finished)
 	call_deferred("_check_unique")
 
 func _check_unique():
@@ -26,7 +45,11 @@ func _check_unique():
 			if i == chest_id:
 				is_open = true
 				reward_given = true
-				body_sprite.frame = 6
+				match chest_type:
+					0:
+						body_sprite.frame = 6
+					1:
+						body_sprite.frame = 6 
 				break
 			else:
 				continue
@@ -43,7 +66,11 @@ func _open_chest():
 		#await get_tree().create_timer(0.4).timeout
 		D.unique_open_data[str(GameManager.current_world.world_name)].append(chest_id)
 		is_open = true
-		anim_player.play("open")
+		match chest_type:
+			0:
+				body_sprite.play("wood")
+			1:
+				body_sprite.play("red")
 
 func _reward_player():
 	if reward_given:
@@ -54,9 +81,8 @@ func _reward_player():
 		var item = reward_list[ randi_range(0, reward_size)-1].instantiate()
 		InventoryManager.add_item(item.item_name, randi_range(1, 4))
 
-func _on_animation_finished(anim_name):
-	if anim_name == "open":
-		_reward_player()
+func _on_animation_finished():
+	_reward_player()
 
 func _process(_delta):
 	if player_detector.can_see_player() and not is_open:
