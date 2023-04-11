c57b5d439ba4f5bf0d5cef1fa7ce588a3e13593a
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-04-11 15:38:07 +0200

    add firetrap @ trap

--- prefab/traps/Spiketrap.gd
+++ prefab/traps/Spiketrap.gd
@@ -1,10 +1,12 @@
 extends Node2D
-
+class_name FloorTrap
 
 @export var trap_damage: int
+@export_enum("Spike", "Fire") var trap_type: int
 @onready var collider = $Hitbox/CollisionShape2D
 @onready var body = $Body
 @onready var player_detector = $PlayerDetector
+@onready var player_collider = $PlayerDetector/CollisionShape2D
 @onready var hitbox = $Hitbox
 
 
@@ -12,21 +14,47 @@ func _ready():
 	body.connect("frame_changed", enable_collider)
 	body.connect("animation_finished", stop_trap)
 	player_detector.connect("body_entered", start_trap)
-	hitbox.damage = trap_damage
+	_setup_trap()
 
+func _setup_trap():
+	hitbox.damage = trap_damage
+	match trap_type:
+		0:
+			body.play("spike_off")
+			body.offset = Vector2(0, -8)
+		1:
+			body.play("fire_off")
+			body.offset = Vector2(0, -12)
 
 func start_trap(b):
 	if b.name != "Player":
-		return 
-	self.body.play("on")
+		return
+	match trap_type:
+		0:
+			self.body.play("spike_on")
+		1:
+			body.play("fire_on")
 
 func stop_trap():
-	body.play("off")
-	if player_detector.can_see_player():
-		body.play("on")
+	match trap_type:
+		0:
+			body.play("spike_off")
+			if player_detector.can_see_player():
+				body.play("spike_on")
+		1: 
+			body.play("fire_off")
+			if player_detector.can_see_player():
+				body.play("fire_on")
 
 func enable_collider():
-	if body.frame == 7:
-		collider.set_deferred("disabled", false)
-	elif body.frame == 11:
-		collider.set_deferred("disabled", true)
+	match trap_type:
+		0:
+			if body.frame == 7:
+				collider.set_deferred("disabled", false)
+			elif body.frame == 11:
+				collider.set_deferred("disabled", true)
+		1:
+			if body.frame == 10:
+				collider.set_deferred("disabled", false)
+			elif body.frame == 11:
+				collider.set_deferred("disabled", true)
