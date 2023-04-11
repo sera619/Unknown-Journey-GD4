916e1c14378423d82906919d73b7a6f000a16ef1
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-04-11 19:02:36 +0200

    Create QuestController.gd

---
+++ world/QuestController.gd
@@ -0,0 +1,29 @@
+extends Node2D
+class_name QuestController
+
+
+@export var quest_name: String
+@export var required_amount: int
+@export var object_ids: Array[int]
+
+var active_objects: Array = []
+
+func _ready():
+	for o in get_children():
+		o.connect("puzzle_piece_on", _object_is_active)
+
+func _object_is_active(id):
+	if active_objects.has(id):
+		return
+	active_objects.append(id)
+	if _check_condition():
+		get_tree().call_group("doors", "unlock_door")
+		if QuestManager.current_quest:
+			var q = QuestManager.current_quest
+			if q.title == "Befreiung" and q.state == Quest.QS.ACTIVE:
+				q.add_item()
+
+func _check_condition() -> bool:
+	if required_amount == active_objects.size():
+		return true
+	return false
