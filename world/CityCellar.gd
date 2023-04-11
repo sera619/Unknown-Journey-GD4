c84c63006d47b7c55bbde2a27ab80c32592ce8b9
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-04-11 19:02:08 +0200

    Add questcheck to kill no needed objects

--- world/CityCellar.gd
+++ world/CityCellar.gd
@@ -6,5 +6,8 @@ func _ready():
 	_on_ready()
 	get_tree().call_group("world_light", "_light_on")
 	lava_damage.damage = 10
+	if QuestManager.is_quest_complete("Befreiung"):
+		$Map/GameObjects/NPC/NPCDanika.queue_free()
+		get_tree().call_group("doors", "unlock_door")
 
 
