c59355993756be00462278654edc22f3709b70e8
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-04-14 14:25:16 +0200

    add attention sound @ accept popup

--- prefab/UI/AcceptPopup.gd
+++ prefab/UI/AcceptPopup.gd
@@ -4,6 +4,7 @@ class_name AcceptPopup
 signal popup_accept
 signal popup_cancel
 
+@export var attention_sound: PackedScene
 @onready var text_label = $BG/M/V/TextBG/Text
 @onready var accept_btn = $BG/M/V/H/OkayBtn
 @onready var back_btn = $BG/M/V/H/BackBtn
@@ -11,13 +12,16 @@ signal popup_cancel
 func _ready():
 	accept_btn.connect("pressed", _accept_pressed)
 	back_btn.connect("pressed", _cancel_pressed)
+	var sound = attention_sound.instantiate()
+	self.add_child(sound)
+	self.show()
 
 func _create_sound():
 	var sound = GameManager.interface.button_click_sound.instantiate()
 	self.add_child(sound)
 
 func set_text(text: String):
-		text_label.text = text 
+	text_label.text = text 
 
 func _accept_pressed():
 	_create_sound()
