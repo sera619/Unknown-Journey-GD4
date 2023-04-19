1b1dae882ed6ad22b9681db15298741c209241a3
S3R43o3 <67480273+sera619@users.noreply.github.com>
2023-04-19 17:30:47 +0200

    fix multiple effect casts

--- prefab/spells/WeaponEffect.gd
+++ prefab/spells/WeaponEffect.gd
@@ -5,6 +5,8 @@ class_name WeaponEffect
 @export_enum("None", "Ice", "Fire", "Lightning", "Poison") var element_type: int
 
 @onready var lightning_impact_sound: PackedScene = preload("res://prefab/audio/LightningImpactSound.tscn")
+@onready var fire_impact_sound: PackedScene = preload("res://prefab/audio/FireImpactSound.tscn")
+@onready var ice_impact_sound: PackedScene = preload("res://prefab/audio/IceImpact.tscn")
 @onready var hitbox = $Hitbox
 @onready var collider = $Hitbox/CollisionShape2D
 @onready var body = $Body
@@ -13,7 +15,7 @@ class_name WeaponEffect
 func _ready():
 	hitbox.damage = effect_damage
 	body.connect("animation_finished", _kill)
-	body.connect("frame_changed", _enable_collider)
+	#body.connect("frame_changed", _enable_collider)
 	_set_element()
 	match effect_element:
 		SkillManager.ELEMENT.NONE:
@@ -22,20 +24,21 @@ func _ready():
 			body.offset = Vector2(0, -128)
 			body.scale = Vector2(0.25, 0.25)
 			body.play("ice_animate")
+			var sound = ice_impact_sound.instantiate()
+			self.add_child(sound)
 		SkillManager.ELEMENT.FIRE:
+			body.offset = Vector2(0, -38)
+			body.scale =Vector2(0.85, 0.85)
 			body.play("fire_animate")
+			var sound = fire_impact_sound.instantiate()
+			self.add_child(sound)
 		SkillManager.ELEMENT.LIGHTNING:
-			var sound = lightning_impact_sound.instantiate()
-			get_tree().current_scene.add_child(sound)
 			body.offset = Vector2(0, -256)
-			body.scale = Vector2(0.15, 0.15)
+			body.scale = Vector2(0.35, 0.35)
 			body.play("lightning_animate")
+			var sound = lightning_impact_sound.instantiate()
+			self.add_child(sound)
 			
-
-func _enable_collider():
-	if body.animation == "ice_animate":
-		if body.frame == 20:
-			collider.set_deferred("disabled", false)
 
 func _kill():
 	self.call_deferred("queue_free")
